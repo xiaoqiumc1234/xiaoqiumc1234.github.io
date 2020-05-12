2302fd2a26b84882638c195039ad9d2f6637a110
xiaoqiumc1234 <64236837+xiaoqiumc1234@users.noreply.github.com>
2020-05-12 12:01:09 +0800

    Add files via upload

---
+++ jitutonglong.cpp
@@ -0,0 +1,17 @@
+﻿#include<iostream>
+#include<string>
+#include<math.h>
+#include<string.h>
+using namespace std;
+int main()
+{
+	int x, y, z, u;
+    //x=头
+    //y=脚
+	x = 35;
+	y = 94;
+	z = 2 * x - y / 2;
+	u = y / 2 - x;
+	cout << "鸡有" << z << "兔有" << u << endl;
+	return 0;
+}
